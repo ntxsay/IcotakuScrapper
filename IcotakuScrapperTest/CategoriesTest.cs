d5564fb838e831b4ef5f5ae52503d24dc791ecdb
ntxsay <ntxsay@hotmail.fr>
2023-12-01 18:02:12 -0400

    Ajout de test

---
+++ IcotakuScrapperTest/CategoriesTest.cs
@@ -0,0 +1,27 @@
+﻿using IcotakuScrapper;
+using IcotakuScrapper.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcotakuScrapperTest
+{
+    public class CategoriesTest
+    {
+
+        [Test]
+        public async Task CreateIndexAsync()
+        {
+            var sections = new HashSet<IcotakuSection>
+            {
+                IcotakuSection.Anime,
+                IcotakuSection.Manga,
+            };
+
+            var result = await Tcategory.CreateIndexAsync(sections);
+            Assert.That(result.IsSuccess, Is.True);
+        }
+    }
+}
