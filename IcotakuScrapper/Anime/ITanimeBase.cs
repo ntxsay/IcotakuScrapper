f8dc090d1d0996e9f50b55bada991217d9d40695
ntxsay <ntxsay@hotmail.fr>
2023-12-27 20:28:54 -0400

    Mise à jourde l'application

---
+++ IcotakuScrapper/Anime/ITanimeBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcotakuScrapper.Anime
+{
+    internal interface ITanimeBase : ITableSheetBase<TanimeBase>
+    {
+        /// <summary>
+        /// Obtient ou définit le guid de l'anime.
+        /// </summary>
+        public Guid Guid { get; }
+    }
+}
