28405079e3ed3ccbf29827622c8993f65c410751
ntxsay <ntxsay@hotmail.fr>
2023-12-30 10:51:41 -0400

    Mise à jour projet

---
+++ IcotakuScrapper/Anime/AnimeAdditionalInfosStruct.cs
@@ -0,0 +1,13 @@
+﻿namespace IcotakuScrapper.Anime
+{
+    /// <summary>
+    /// Structure contenante les informations additionnelles d'un anime.
+    /// </summary>
+    internal readonly struct AnimeAdditionalInfosStruct
+    {
+        internal int SheetId { get; init; }
+        internal string? ThumbnailUrl { get; init; }
+        internal bool IsAdultContent { get; init; }
+        internal bool IsExplicitContent { get; init; }
+    }
+}
