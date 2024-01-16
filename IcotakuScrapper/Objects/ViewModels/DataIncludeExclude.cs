d6220dbda66f8adf42580ad6e7621fbb979e3021
ntxsay <ntxsay@hotmail.fr>
2024-01-15 20:16:01 -0400

    Mise à jour de l'animé

---
+++ IcotakuScrapper/Objects/ViewModels/DataIncludeExclude.cs
@@ -0,0 +1,162 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace IcotakuScrapper.Objects.ViewModels
+{
+    /// <summary>
+    /// Enumération permettant de gérer les données à inclure ou exclure dans une recherche
+    /// </summary>
+    public enum IncludeDataMode
+    {
+        /// <summary>
+        /// Que l'élément apparaisse ou non dans les résultats importe peu.
+        /// </summary>
+        Ignore,
+        /// <summary>
+        /// La données est incluse dans la recherche de résultats
+        /// </summary>
+        Include,
+        /// <summary>
+        /// La données est exclue de la recherche de résultats
+        /// </summary>
+        Exclude,
+    }
+
+    /// <summary>
+    /// Classe permettant de gérer les données à inclure ou exclure dans une recherche
+    /// </summary>
+    public class DataIncludeExclude : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged = delegate { };
+
+        public DataIncludeExclude()
+        {
+
+        }
+
+        public DataIncludeExclude(IncludeDataMode mode, string header, object? data, string? description)
+        {
+            Mode = mode;
+            Header = header;
+            Data = data;
+            Description = description;
+        }
+
+        public DataIncludeExclude(object? data, string header, string? description)
+        {
+            Data = data;
+            Header = header;
+            Description = description;
+        }
+
+        protected IncludeDataMode _Mode;
+        public virtual IncludeDataMode Mode
+        {
+            get => _Mode;
+            set
+            {
+                if (_Mode != value)
+                {
+                    _Mode = value;
+                    _IsIncluded = value switch
+                    {
+                        IncludeDataMode.Ignore => null,
+                        IncludeDataMode.Include => true,
+                        IncludeDataMode.Exclude => false,
+                        _ => null,
+                    };
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        protected bool? _IsIncluded;
+        public virtual bool? IsIncluded
+        {
+            get => _IsIncluded;
+            set
+            {
+                if (_IsIncluded != value)
+                {
+                    _IsIncluded = value;
+                    _Mode = value switch
+                    {
+                        null => IncludeDataMode.Ignore,
+                        true => IncludeDataMode.Include,
+                        false => IncludeDataMode.Exclude,
+                    };
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        protected string _Header = string.Empty;
+        public virtual string Header
+        {
+            get => _Header;
+            set
+            {
+                if (_Header != value)
+                {
+                    _Header = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        protected object? _Data;
+        public virtual object? Data
+        {
+            get => _Data;
+            set
+            {
+                if (_Data != value)
+                {
+                    _Data = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        protected string? _Description;
+        public virtual string? Description
+        {
+            get => _Description;
+            set
+            {
+                if (_Description != value)
+                {
+                    _Description = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    /// <summary>
+    /// Structure permettant de gérer les données à inclure ou exclure dans une recherche
+    /// </summary>
+    public readonly struct DataIncludeExcludeStruct
+    {
+        public IncludeDataMode Mode { get; init; }
+        public string Header { get; init; } = "";
+        public object? Data { get; init; }
+        public string? Description { get; init; }
+
+        public DataIncludeExcludeStruct()
+        {
+
+        }
+
+        public DataIncludeExcludeStruct(IncludeDataMode mode, string header, object? data, string? description)
+        {
+            Mode = mode;
+            Header = header;
+            Data = data;
+            Description = description;
+        }
+    }
+}
---
+++ IcotakuScrapper/Objects/ViewModels/StatusDiffusionSelector.cs
@@ -0,0 +1,107 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace IcotakuScrapper.Objects.ViewModels;
+
+
+
+public readonly struct StatusDiffusionSelectorStruct
+{
+    public DiffusionStateKind Kind { get; init; }
+    public string Name { get; init; } = null!;
+
+    public StatusDiffusionSelectorStruct(DiffusionStateKind kind, string name)
+    {
+        Kind = kind;
+        Name = name;
+    }
+
+    public StatusDiffusionSelectorStruct()
+    {
+
+    }
+}
+
+public class StatusDiffusionSelector : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged = delegate { };
+    private DiffusionStateKind _Kind;
+
+    public DiffusionStateKind Kind
+    {
+        get => _Kind;
+        set
+        {
+            if (_Kind != value)
+            {
+                _Kind = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private string _Name = null!;
+
+    public string Name
+    {
+        get => _Name;
+        set
+        {
+            if (_Name != value)
+            {
+                _Name = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    internal StatusDiffusionSelector(DiffusionStateKind kind, string name)
+    {
+        Kind = kind;
+        Name = name;
+    }
+
+    internal StatusDiffusionSelector()
+    {
+    }
+
+    public static IEnumerable<StatusDiffusionSelector> GetFilterByList()
+    {
+        var enumValues = Enum.GetValues<DiffusionStateKind>().ToArray();
+        foreach (var enumValue in enumValues)
+        {
+            yield return enumValue switch
+            {
+                DiffusionStateKind.Unknown => new StatusDiffusionSelector(enumValue, "Inconnu"),
+                DiffusionStateKind.UpComing => new StatusDiffusionSelector(enumValue, "Bientôt"),
+                DiffusionStateKind.InProgress => new StatusDiffusionSelector(enumValue, "En cours"),
+                DiffusionStateKind.Paused => new StatusDiffusionSelector(enumValue, "En Pause"),
+                DiffusionStateKind.Stopped => new StatusDiffusionSelector(enumValue, "Annulé"),
+                DiffusionStateKind.Completed => new StatusDiffusionSelector(enumValue, "Terminé"),
+                _ => throw new NotImplementedException(),
+            };
+        }
+    }
+
+    public static IEnumerable<StatusDiffusionSelectorStruct> GetFilterStructByList()
+    {
+        var enumValues = Enum.GetValues<DiffusionStateKind>().ToArray();
+        foreach (var enumValue in enumValues)
+        {
+            yield return enumValue switch
+            {
+                DiffusionStateKind.Unknown => new StatusDiffusionSelectorStruct(enumValue, "Inconnu"),
+                DiffusionStateKind.UpComing => new StatusDiffusionSelectorStruct(enumValue, "Bientôt"),
+                DiffusionStateKind.InProgress => new StatusDiffusionSelectorStruct(enumValue, "En cours"),
+                DiffusionStateKind.Paused => new StatusDiffusionSelectorStruct(enumValue, "En Pause"),
+                DiffusionStateKind.Stopped => new StatusDiffusionSelectorStruct(enumValue, "Annulé"),
+                DiffusionStateKind.Completed => new StatusDiffusionSelectorStruct(enumValue, "Terminé"),
+                _ => throw new NotImplementedException(),
+            };
+        }
+    }
+
+    public virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
+
